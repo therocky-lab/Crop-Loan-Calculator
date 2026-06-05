8b8e0fbf03983611e1f3a707ad6c6d4d4ad1ad60
therocky-lab <therockyg097@gmail.com>
2026-06-05 09:50:23 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = "crop-loan-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
